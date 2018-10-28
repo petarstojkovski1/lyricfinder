afa6f2dd1e33ccaf55d73475e364dc3703dcdd57
Petar <stojkovskipetar@gmail.com>
2018-10-28 22:04:30 +0100

    Updates

---
+++ precache-manifest.a0d63f4fa7198f084b8bf5ad5c90e2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "404cd28e19c4c9466cf3",
+    "url": "/lyricfinder/static/js/runtime~main.404cd28e.js"
+  },
+  {
+    "revision": "e9e308232bb0a449b725",
+    "url": "/lyricfinder/static/js/main.e9e30823.chunk.js"
+  },
+  {
+    "revision": "f4337b640ffa95c163ca",
+    "url": "/lyricfinder/static/js/1.f4337b64.chunk.js"
+  },
+  {
+    "revision": "e9e308232bb0a449b725",
+    "url": "/lyricfinder/static/css/main.6f767a65.chunk.css"
+  },
+  {
+    "revision": "cc1ae4c9c2f790a74f313b57f9262965",
+    "url": "/lyricfinder/index.html"
+  }
+];
